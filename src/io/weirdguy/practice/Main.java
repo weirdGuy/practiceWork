090a8d2b21d69d29cfed311763baf6619543d33e
Artem <laiko2012@yandex.ua>
2017-02-09 18:30:03 +0200

    upd

--- src/io/weirdguy/practice/Main.java
+++ src/io/weirdguy/practice/Main.java
@@ -1,6 +1,6 @@
 package io.weirdguy.practice;
 
-import io.weirdguy.practice.sorters.MergeSort;
+import io.weirdguy.practice.sorters.*;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -11,21 +11,28 @@ public class Main {
 
         Item[] items = new Item[10];
 
+        SortStrategy[] sorters = new SortStrategy[]{new CombSort(), new HeapSort(),
+                new InsertionSort(), new MergeSort(), new QuickSort(), new SelectionSort(), new ShellSort()};
+
         Random rn = new Random();
         int n = 100 - 1 + 1;
 
         for(int i = 0; i < items.length; i++) {
             items[i] = new Item(rn.nextInt() % n, rn.nextInt() % n, "RGB" + rn.nextInt() % n + "6" + i);
         }
-        StrategyContext sorter = new StrategyContext(new MergeSort());
+        StrategyContext sorter = new StrategyContext(new BubbleSort());
+
+        Item[] copy = Arrays.copyOf(items, items.length);
 
         log(Arrays.toString(items));
 
-        sorter.sortAsc(items);
-        //Sorter.quickSort(items);
-
-        log(Arrays.toString(items));
-
+        for (SortStrategy s:
+             sorters) {
+            sorter.changeStrategy(s);
+            sorter.sortAsc(items);
+            log(Arrays.toString(items));
+            items = Arrays.copyOf(copy, copy.length);
+        }
 
     }
 
